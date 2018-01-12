b9bbfbd2152be216dce7a6c12280edd37c4ce068
Red-French <red@redfrench.net>
2018-01-12 17:26:33 -0600

    change to event listeners

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
 $( document ).ready(function() {
 
-  $("a").click(function(event) {
-    
-    let chosenPic = event.target.name + ".jpg"
-    let chosenPicLocation = './img/' + chosenPic;
-    let carMake = event.target.alt;
-    let intro = document.getElementsByClassName('intro')[0];
+  const imgLinks = $(".carImage");
+  let i = imgLinks.length;
 
-    if(intro) {intro.remove();};
+  while (i--) {
+    imgLinks[i].addEventListener("click", handleCarSelectionClick);
+  }
+
+  function handleCarSelectionClick(event) {
+    let chosenPic = event.srcElement.name + ".jpg";;
+    let chosenPicLocation = './img/' + chosenPic;
+    let carMakeCaption = event.target.alt;
+    let intro = $(".intro");
+
+    if(intro) {intro.remove();};  // remove intro text
+
+    // load chosen pic/caption
+    $(".detail-image").attr("src", chosenPicLocation);
+    $(".detail-image-title").html(carMakeCaption);
+  }
+
+  // WITHOUT EVENT LISTENERS
+  // $("a").click(function(event) {
+  //   let chosenPic = event.target.name + ".jpg"
+  //   let chosenPicLocation = './img/' + chosenPic;
+  //   let carMakeCaption = event.target.alt;
+  //   let intro = document.getElementsByClassName('intro')[0];
+
+  //   if(intro) {intro.remove();};
     
-    document.getElementsByClassName('detail-image')[0].src = chosenPicLocation;
-    document.getElementsByClassName('detail-image-title')[0].innerHTML = carMake;
-  });
+  //   document.getElementsByClassName('detail-image')[0].src = chosenPicLocation;
+  //   document.getElementsByClassName('detail-image-title')[0].innerHTML = carMakeCaption;
+  // });
 
 });
